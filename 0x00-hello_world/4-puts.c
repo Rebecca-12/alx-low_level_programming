ffc81e3ceb496e4e2e32a177614bb42874d47ec2
Rebecca-12 <essandohrebecca@gmail.com>
2023-02-17 13:31:59 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+include <stdio.h>
 /*
  * main - printsthe phrase "Programming is like building a multilingual puzzle"
  * Return: 0 if exited properly ,  none-zero otherwise
  * */
 
 int main(){
-	 puts("\"Programming is like building a multilingual puzzle\");
+	 puts("\"Programming is like building a multilingual puzzle\"");
 
 	 return (0);
 }
