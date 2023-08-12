3b395443d36bfe23d53730b95a29f6f15d1823f6
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-08-12 11:47:55 -0700

    First task on c-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-include <stdio.h>
-/*
- * main - printsthe phrase "Programming is like building a multilingual puzzle"
- * Return: 0 if exited properly ,  none-zero otherwise
- * */
-
-int main(){
-	 puts("\"Programming is like building a multilingual puzzle\"");
-
-	 return (0);
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ **/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
-
-
