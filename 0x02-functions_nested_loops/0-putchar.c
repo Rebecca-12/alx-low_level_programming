1efebce9b2f97f54ffe240dd1074f595d263e7d7
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-08-20 22:08:42 -0700

    nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - print _putchar 
- * is written by mbah
- * Return : 0 
+ * main - prints _putchar
+ * followed by a new line.
+ * Return : 0
  */
 
 int main(void)
