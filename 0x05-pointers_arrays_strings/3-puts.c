30591a8536ecebfa7ddd8f2a8de6755f8fa5a49a
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-08-23 08:14:51 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-|#include "main.h"
+#include "main.h"
 
 /*
 *_puts - prints a string, followed by a new line, to stdout
@@ -12,7 +12,8 @@ void _puts(char *str)
 	while (*str != '\0')
 	{
 
-		_putchar(*str++);
+		_putchar
+
 
 	}
 		_putchar('\n');
