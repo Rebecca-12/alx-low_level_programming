4b76702af4c731a955e59d76967d6566f84b0a7d
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-09-11 22:35:06 -0700

    My very first commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* new_dog - Creates a new dog_t structure
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*
+* Return: Pointer to the new dog_t structure, or NULL on failure
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog_ptr;
+	char *name_copy, *owner_copy;
+
+	/* Allocate memory for the new dog_t structure */
+	new_dog_ptr = malloc(sizeof(dog_t));
+
+	if (new_dog_ptr == NULL)
+		return (NULL);
+
+	/* Allocate memory for the name and owner strings and copy the data */
+	name_copy = strdup(name);
+	owner_copy = strdup(owner);
+
+	if (name_copy == NULL || owner_copy == NULL)
+	{
+		free(name_copy);
+		free(owner_copy);
+		free(new_dog_ptr);
+		return (NULL);
+	}
+
+	/* Assign the copied strings and age to the new dog_t structure */
+	new_dog_ptr->name = name_copy;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = owner_copy;
+
+	return (new_dog_ptr);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/* Define a new struct dog with three elements */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+/* Define a new name dog_t for the struct dog type */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
