20704a5ffea412687b98862a5eced0703ad980f5
Rebecca-12 <rebeckietaessandoh@gmail.com>
2023-09-16 06:14:43 -0700

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,17 @@
 #include <stdarg.h>
 
+/**
+* sum_them_all - Returns the sum of a variable number of integers.
+* @n: The number of integers to be summed.
+*
+* Description:
+* This function takes a variable number of integer arguments and returns
+* their sum. It uses the concept of variadic functions in C to handle
+* a flexible number of arguments.
+*
+* Return: The sum of the integers.
+*/
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
