6abc036165586dd45d71de5f0ce6af25b1937c5c
Rebecca-12 <rebeckietaesandoh@gmail.com>
2023-09-29 10:44:46 -0700

    First commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_
+#include <unistd.h>
 
 /**
 * binary_to_uint - Converts a binary number to an unsigned int.
@@ -16,6 +17,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
