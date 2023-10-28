390ca5ead3fb423a22983a8068c581060a057908
Rebecca_Essandoh <rebeckietaessandohb@gmail.com>
2023-10-28 15:00:39 -0700

    Initial Commit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,7 +2,9 @@
 #include "main.h"
 
 /**
-* flip_bits - Returns the number of bits to flip to get from one number to another.
+* flip_bits - Returns the number
+*  of bits to flip to get
+*  from one number to another.
 * @n: First unsigned long integer
 * @m: Second unsigned long integer
 *
