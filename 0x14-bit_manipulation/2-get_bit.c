c55a295ba428febcc151195d27c7a6092937a0c8
Rebecca_Essandoh <rebeckietaessandohb@gmail.com>
2023-10-27 21:27:08 -0700

    Initial commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,19 @@
-
 #include "main.h"
 
 /**
-* clear_bit - Sets the value of a bit to 0 at a given index.
-* @n: Pointer to the unsigned long integer
-* @index: Index of the bit to clear (starting from 0)
+* get_bit - Returns the value of a bit at a given index.
+* @n: Unsigned long integer.
+* @index: Index, starting from 0, of the bit to get.
 *
-* Return: 1 if it worked, or -1 if an error occurred.
+* Return: The value of the bit at index, or -1 if an error occurred.
 */
 
-int clear_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
+    /* Check if index is within the valid range */
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n = *n & ~(1 << index);
-	return (1);
+    /* Shift n to the right by index and check the least significant bit */
+	return ((n >> index) & 1);
 }
