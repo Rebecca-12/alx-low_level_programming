5c7e571a25aa84332d5d99cf53ea2517713e8a08
Rebecca_Essandoh <rebeckietaessandohb@gmail.com>
2023-10-30 23:05:50 -0700

    Initial Commit

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>  /* For size_t */
+#include <unistd.h>  /* For ssize_t */
+
+/* Function prototypes */
+
+/* Task 0 */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Task 1 */
+int create_file(const char *filename, char *text_content);
+
+/* Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* Task 3 */
+int cp(const char *file_from, const char *file_to);
+
+/* Task 4 - Advanced */
+void elf_header(const char *elf_filename);
+
+/* Helper function for printing characters */
+int _putchar(char c);
+
+#endif /* MAIN_H */
