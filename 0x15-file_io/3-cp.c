fa6e9d27132a1c94de0a82699b9a441ee20697d4
Rebecca_Essandoh <rebeckietaessandohb@gmail.com>
2023-11-03 23:38:48 -0700

    Initial commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,69 +1,76 @@
+
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
-* copy_file - Copies the content of one file to another.        * @file_from: The name of the source file.
-* @file_to: The name of the destination file.
-* Return: 0 on success, otherwise:                              *         97 for incorrect number of arguments
-*         98 if file_from cannot be read
-*         99 if file_to cannot be written or write fails        *         100 if file descriptor cannot be closed
-*/
-
 int copy_file(const char *file_from, const char *file_to)
 {
-        int fd_from, fd_to, bytes_read, bytes_written;
-        char buffer[1024];
+    int fd_from, fd_to, bytes_read, bytes_written;
+    char buffer[1024];
 
-        if (file_from == NULL || file_to == NULL)
-                return (97);
+    if (file_from == NULL || file_to == NULL)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        return 97;
+    }
 
-        fd_from = open(file_from, O_RDONLY);
-        if (fd_from == -1)
+    fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+        return 98;
+    }
+
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (fd_to == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+        close(fd_from);
+        return 99;
+    }
+
+    while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+    {
+        bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written != bytes_read)
         {
-                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-                return (98);
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+            close(fd_from);
+            close(fd_to);
+            return 99;
         }
+    }
 
-        fd_to = open(file_to,
-        O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-        if (fd_to == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-                close(fd_from);
-                return (99);
-        }
-        while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-        {
-                bytes_written = write(fd_to, buffer, bytes_read);
-                if (bytes_written != bytes_read)
-                {
-                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-                        close(fd_from);
-                        close(fd_to);
-                        return (99);
+    if (bytes_read == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+        close(fd_from);
+        close(fd_to);
+        return 98;
+    }
 
-                }
-        }
+    if (close(fd_from) == -1 || close(fd_to) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+        return 100;
+    }
 
+    return 0;
+}
 
-        if (bytes_read == -1)
-        {
-                dprintf(STDERR_FILENO, "Error reading from file %s\n", file_from);
-                close(fd_from);
-                close(fd_to);
-                return (98);
-        }
+int main(int argc, char *argv[])
+{
+    int status; /* Move the declaration of the 'status' variable to the beginning*/
 
-        if (close(fd_from) == -1 || close(fd_to) == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-                return (100);
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+        return 97;
+    }
 
-	}
+    status = copy_file(argv[1], argv[2]); /* Assign the value to the 'status' variable*/
 
-	return (0);
-
+    return status;
 }
